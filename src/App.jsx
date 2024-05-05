33d5e091d1cfb54a88501c37aff287ddfc216d10
Vicky Dhote <dhoteji034@gmail.com>
2024-05-05 14:07:32 +0530

    form

--- src/App.jsx
+++ src/App.jsx
@@ -2,59 +2,46 @@ import { useState } from "react"
 
 
 const App=()=>{
-  // const [age, setAge] = useState(12);
-  // const Agechange=()=>{
-  //   setAge(13);
-  //   console.log(age);}
   const [username, setUsername] = useState(""); 
   const [gender, setGender] = useState("");
   const [city, setCity] = useState("");
+  const [description,setText]=useState("");
+  const [checkedBox,setCheckBox]=useState("");
   const[accept, setAccept] = useState("");
-  const SubmiteHandler=(e)=>{
-      e.preventDefult();
-      console.log({username});
+
+
+  const SubmiteHandler=(e)=>{e.preventDefult();
+    console.log(username)
+    console.log(gender)
+    console.log(city)
+    console.log(description)
+    console.log(checkedBox)
     }
 
 return(
 <div>
-  {/* <h1>{Age}</h1>
-  <button onClick={Agechange}>change Age</button> */}
-
   <h1>TWO WAY BINDINNG</h1>
-  <form >
-    <input type="text"  onChange={(e)=>
-     setUsername(e.target.value)
-    } 
-    value={username}
-     placeholder="username" id="name" />
-     <br />
-      <input type="radio"
-      onChange={(e)=>setGender(e.target.value)} 
-      checked={gender=="male"?true:false } 
-      name="gender" 
-      value="male"/>
-      male
-    <input type="radio"
-    onChange={(e)=>setGender(e.target.value)} 
-      checked={gender=="female"?true:false } 
-       name="gender"
-       value="female" />
-     female
-  <br />
-  <select defaultValue={city}
-  onChange={(e)=>setGender(e.target.value)}>
-    <option value="delhi">delhi</option>
+  <form onSubmit={SubmiteHandler} >
+   <input type="text"onChange={(e)=>{setUsername(e.target.value)}}
+    value={username} placeholder="username" id="username" />
+     <br /> <br />
+       <input type="radio"onChange={(e)=>setGender(e.target.value)}checked={gender=="male"?true:false } 
+       name="gender"value="male"/>male
+       <input type="radio"onChange={(e)=>setGender(e.target.value)}checked={gender=="female"?true:false } 
+       name="gender"value="female" />female
+  <br /> <br />
+     <select defaultValue={city}onChange={(e)=>setCity(e.target.value)}>
+     <option value="delhi">delhi</option>
     <option value="kolkata">kolkata</option>
     <option value="mumbai">mumbai</option>
     <option value="pune">pune</option>
-  </select>
-  <br /> <br />
-   <input type="checkbox"
-  //  value={ option1}
-   onChange={(e)=>setAccept(e.target.checked)} 
- />  Accept
- <br /> <br />
-  <input type="submit"  onChange={(e)}value="submit" />
+    </select>
+   <br /> <br />
+     <textarea placeholder="Enter text here" onChange={(e)=>setText(e.target.value)} value={description} ></textarea>
+   <br /> <br />
+     <input type="checkbox" onChange={(e)=>{setCheckBox(e.target.checked)}} value={checkedBox} checked={checkedBox?true:false}  />Select All
+   <br /> <br />
+    <input type="submit" value="submit" />
   </form>
 </div>
 )}
