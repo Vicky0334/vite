1ac1043ae82db19a9ccabdb26ae4273adda18f6c
Vicky Dhote <dhoteji034@gmail.com>
2024-05-06 14:45:53 +0530

    add desc

--- src/App.jsx
+++ src/App.jsx
@@ -10,89 +10,68 @@ const App = () => {
     const [accept, setAccept] = useState(false);
     const SubmitHandler = (e) => {
         e.preventDefault();
-
-        const newuser = { username, gender, city, desc };
-
+        const newuser = { username, gender, city, desc,accept};
         const copyuser = [...users];
         copyuser.push(newuser);
         setusers(copyuser);
-
-        // setusers([...users, newuser])
-
-        setUsername("");
+       // setusers([...users, newuser])
+        // setUsername("");
         setCity("mumbai");
         setGender("male");
         setDesc("");
         setAccept(false);
     };
-
-    let listrender =
-        users.length > 0 ? (
-            users.map((user, index) => {
+     let listrender =users.length > 0 ?(users.map((user, index) => {
                 return (
                     <div key={index}>
                         <h1>{user.username}</h1>
-                        {user.city}
+                        <p>{user.gender}</p>
+                        <h2>{user.city}</h2>
+                        <h2>{user.desc}</h2>
                     </div>
-                );
-            })
-        ) : (
+                );})) : 
+             (
             <h1>no data present</h1>
-        );
+            );
 
     return (
         <div>
             <h1>Two Way Binding</h1>
             <form onSubmit={SubmitHandler}>
-                <input
-                    onChange={(e) => setUsername(e.target.value)}
-                    value={username}
+               <input onChange={(e) =>setUsername(e.target.value)}value={username}
                     type="text"
                     placeholder="username"
                 />
                 <br /> <br />
-                <input
-                    onChange={(e) => setGender(e.target.value)}
-                    value="male"
-                    checked={gender === "male" ? true : false}
+                <input onChange={(e) => setGender(e.target.value)}
+                   checked={gender === "male" ? true : false}
+                   value="male"
                     type="radio"
-                    name="gender"
-                />
-                Male
-                <input
-                    onChange={(e) => setGender(e.target.value)}
-                    value="female"
+                    name="gender"/>Male
+                <input onChange={(e) => setGender(e.target.value)}
                     checked={gender === "female" ? true : false}
+                    value="female"
                     type="radio"
-                    name="gender"
-                />
-                Female
+                    name="gender"/>Female
                 <br /> <br />
-                <select
-                    defaultValue={city}
-                    onChange={(e) => setCity(e.target.value)}
-                >
+                <select defaultValue={city}
+                onChange={(e) => setCity(e.target.value)}>
                     <option value="delhi">Delhi</option>
                     <option value="pune">Pune</option>
                     <option value="mumbai">Mumbai</option>
                     <option value="kolkata">Kolkata</option>
                 </select>
                 <br /> <br />
-                <textarea
-                    onChange={(e) => setDesc(e.target.value)}
-                    value={desc}
+                <textarea onChange={(e) => setDesc(e.target.value)}
+                   value={desc}
                     placeholder="enter text here"
                 ></textarea>
                 <br /> <br />
-                <input
-                    // value="Option 1"
-                    checked={accept ? true : false}
+                <input checked={accept ? true : false}
                     onChange={(e) => setAccept(e.target.checked)}
                     type="checkbox"
-                />
-                Accept
-                <br />
-                <br />
+                /> Accept
+                <br /><br />
                 <button>Submit</button>
             </form>
             <hr />
